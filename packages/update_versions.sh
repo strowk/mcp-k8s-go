65b3a06e35cd710234f1ca87f42b94cbaf37a2ac
Timur Sultanaev <str.write@gmail.com>
2024-12-01 16:12:51 +0100

    improve version updating

--- packages/update_versions.sh
+++ packages/update_versions.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-previous_version="0.0.9"
-new_version="0.0.10"
+previous_version="$(npm view ./packages/npm-mcp-k8s version)"
+new_version="${1}"
+
+if [ -z "$new_version" ]; then
+  echo "Usage: $0 <new_version>"
+  exit 1
+fi
 
 # replace previous version with new version in all .json files in ./packages folder 
 find ./packages -type f -name '*.json' -exec  sed -i '' -e "s/${previous_version}/${new_version}/g" {} \;
