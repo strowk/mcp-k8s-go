8b745bca893acdc4931582e126a5335933621d3f
Timur Sultanaev <str.write@gmail.com>
2024-12-01 15:25:57 +0100

    correct workflow filter

--- packages/update_versions.sh
+++ packages/update_versions.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+previous_version="0.0.9"
+new_version="0.0.10"
+
 # replace previous version with new version in all .json files in ./packages folder 
-find ./packages -type f -name '*.json' -exec  sed -i '' -e 's/0.0.9/0.0.10/g' {} \;
+find ./packages -type f -name '*.json' -exec  sed -i '' -e "s/${previous_version}/${new_version}/g" {} \;
 
