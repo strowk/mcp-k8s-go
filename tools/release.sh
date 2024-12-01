38ac36709d5511a0fb3feb5b796e29d56236eb04
Timur Sultanaev <str.write@gmail.com>
2024-12-01 16:22:10 +0100

    no need for another push

--- tools/release.sh
+++ tools/release.sh
@@ -21,7 +21,3 @@ git commit -m "chore: update npm packages versions to $new_version"
 
 git tag -a "v$new_version" -m "release v$new_version"
 goreleaser release --clean
-
-git push origin "v$new_version"
-
-
