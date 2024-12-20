2f20d0f9673b41604fd601b929910e109ec8816a
Timur Sultanaev <str.write@gmail.com>
2024-12-20 20:25:03 +0100

    allow preleleases in release.sh

--- tools/release.sh
+++ tools/release.sh
@@ -13,16 +13,15 @@ fi
 # drop the v prefix
 new_version="${new_version#v}"
 
-# check that new version is X.Y.Z
-if [[ ! $new_version =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
-  echo "Version should be in format X.Y.Z where X, Y, Z are numbers"
+# check that new version is X.Y.Z or X.Y.Z-beta.N
+if ! echo "$new_version" | grep -E '^[0-9]+\.[0-9]+\.[0-9]+(-beta\.[0-9]+)?$' > /dev/null; then
+  echo "Version should be in format X.Y.Z where X, Y, Z are numbers, or X.Y.Z-beta.N"
   exit 1
 fi
 
 packages/update_versions.sh $new_version
 git add ./packages
-git commit -m "chore: update npm packages versions to $new_version"
-git push
+git commit -m "chore: update npm packages versions to $new_version" && git push || true
 
 git tag -a "v$new_version" -m "release v$new_version"
 git push origin "v$new_version"
