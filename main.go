b7702a42ce795c8e5bb0517db8d3058af90e5401
strowk <str.write@gmail.com>
2025-11-09 17:07:39 +0100

    Update help message for --mask-secrets option

--- main.go
+++ main.go
@@ -94,7 +94,7 @@ func printHelp() {
 	println("  --readonly: Disables any tool which can write changes to the cluster")
 	println("      If not specified, all tools are available")
 	println("  --mask-secrets: Mask secrets in the output")
-	println("      If not specified, secrets are returned unchanged")
+	println("      If not specified, secrets would be masked, provide false to opt-out")
 }
 
 func getApp() *app.Builder {
