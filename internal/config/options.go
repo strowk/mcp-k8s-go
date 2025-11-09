8e4ca5d19ceefcea78b15a896dbdd87ea4b75f66
strowk <str.write@gmail.com>
2025-11-09 17:06:41 +0100

    Update default for mask-secrets flag
    Change default value of 'mask-secrets' flag to true to be secure by default.

--- internal/config/options.go
+++ internal/config/options.go
@@ -28,7 +28,7 @@ func ParseFlags() bool {
 	var allowedContextsStr string
 	flag.StringVar(&allowedContextsStr, "allowed-contexts", "", "Comma-separated list of allowed k8s contexts. If empty, all contexts are allowed")
 	flag.BoolVar(&GlobalOptions.Readonly, "readonly", false, "Disables any tool which can write changes to the cluster. If not specified, all tools are allowed")
-	flag.BoolVar(&GlobalOptions.MaskSecrets, "mask-secrets", false, "Mask secrets in the output. If not specified, secrets are returned unchanged")
+	flag.BoolVar(&GlobalOptions.MaskSecrets, "mask-secrets", true, "Mask secrets in the output. If not specified, secrets would be masked, provide false to opt-out")
 
 	// Add other flags here
 
